77e359c05d5ba689608c88c3f4eef49013f9d1de
Luan de Barros <luanbarrossantos@gmail.com>
2024-04-01 22:54:46 -0300

    falta bastante coisa do código ainda pra terminar

--- avaliacoes/avaliacao1/aval1_q4.m
+++ avaliacoes/avaliacao1/aval1_q4.m
@@ -28,12 +28,25 @@ X(idx4) = unifrnd(0,20,1,sum(idx4));
 
 dx= 1/3; x=0:dx:20;
 
+% PDF
 pdfX_sim = hist(X,x) / (N*dx);
-pdfX_teo = %TODO HERE
+pdfX_teo = (1/60) .* (0 < x & x < 20); % sem os impulsos
+
+% CDF
+% cdfX_sim = cumsum(pdfX_sim)*dx;
 
 figure;
 subplot(2,1,1); hold on; grid on;
-xlim([0 20]); ylim([0 1/3];)
-xlabel('X'); ylabel('PDF_(X)');
+bar(x,pdfX_sim, 'y');
+plot(x, pdfX_teo, 'b', 'LineWidth',4);
+
+%  representando os impulsos
+plot([10, 10], [0, 1/3], 'b', 'LineWidth', 3');
+plot([10], [1/3], 'b^', 'MarkerSize', 12, 'MarkerFaceColor', 'b');
+plot([5, 5], [0, 1/3], 'b', 'LineWidth', 3');
+plot([5], [1/3], 'b^', 'MarkerSize', 12, 'MarkerFaceColor', 'b');
+plot([0, 0], [0, 1/6], 'b', 'LineWidth', 3');
+plot([0], [1/6], 'b^', 'MarkerSize', 12, 'MarkerFaceColor', 'b');
+xlim([0 20]); ylim([0 1/3]);
+xlabel('X'); ylabel('PDF(X)');
 title('PDF');
-bar(x,pdfX_sim);
