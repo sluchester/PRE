b6f80923873fcb1807b519da639d20ab51b7242e
Luan de Barros <luanbarrossantos@gmail.com>
2024-07-14 20:03:00 -0300

    Resultados chegaram próximos do calculado

--- avaliacoes/avaliacao4/aval4.m
+++ avaliacoes/avaliacao4/aval4.m
@@ -3,7 +3,37 @@
 clc; close all; clear all;
 
 % numero de realizações
-N = 10;
+N = 1000000;
 
 X1 = 3*rand(1,N);
-X2 =  rand(1,N);
+X2 = 3*rand(1,N);
+
+Y1 = X1.^2; 
+Y2 = X2.^2;
+Y3 = X1.*X2;
+
+Z1 = Y1;
+Z2 = Y1 + Y2;
+Z3 = Y1 + Y2 + Y3;
+
+% Letra A
+% Vetor média de Y
+VetMediaY_sim = mean([Y1' Y2' Y3'])'
+VetMediaY_teo = [3;3;9/4]
+
+% Matriz covariância de Y
+MatrizCovY_sim = cov([Y1' Y2' Y3'])'
+MatrizCovY_teo = [66/5, 0, 27/8;
+                  0, 66/5, 27/8;
+                  27/8, 27/8,63/16]
+
+% Letra B
+% Vetor média de Z
+VetMediaZ_sim = mean([Z1' Z2' Z3'])'
+VetMediaZ_teo = [3,6,33/4]'
+
+% Matriz covariância do vetor aleatório Z
+MatrizCovZ_sim = cov([Z1' Z2' Z3'])'
+MatrizCovZ_teo = [66/5, 66/5, 66/5;
+                  66/5, 132/5, 132/5;
+                  663/40, 663/20, 663/20]
